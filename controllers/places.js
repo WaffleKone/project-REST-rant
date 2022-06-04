4b4c7f0128349541e5a7db13cbfc81bc3173c5d4
Waffle <jaxonjjohnson@gmail.com>
2022-06-04 09:41:45 -0400

    added post route and places model

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-
+const places = require('../models/places.js')
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
@@ -21,6 +21,11 @@ router.get('/', (req, res) => {
       }]
       
     res.render('places/index', { places })
+})
+
+router.post('/', (req, res) => {
+  console.log(req.body)
+  res.send('POST /places')
 })
 
 module.exports = router
---
+++ views/places/post.jsx
@@ -0,0 +1,15 @@
+const React = require('react')
+const Def = require('../default')
+
+function post () {
+    return (
+      <Def>
+          <main>
+              <h1>places post stub</h1>
+          </main>
+      </Def>
+    )
+  }
+  
+
+module.exports = post
