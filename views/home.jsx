6492e8d8f3c06f1ee2343ec200acbef7602a315b
Waffle <jaxonjjohnson@gmail.com>
2022-06-04 09:10:36 -0400

    added static images

--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,15 @@ function home () {
       <Def>
           <main>
               <h1>HOME</h1>
+              <div>
+                <img src="/images/foodimage.jpg" alt="Yummy Food"/>
+                <div>
+                  Photo by <a href="https://unsplash.com/@brookelark">Brooke Lark</a> on <a href="https://unsplash.com">Unsplash</a>
+                </div>
+              </div>
+              <a href="/places">
+                <button className="btn-primary">Places Page</button>
+              </a>
           </main>
       </Def>
     )
